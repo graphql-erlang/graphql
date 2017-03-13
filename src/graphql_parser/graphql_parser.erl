a5712ea91109148ae3a263fff441602e1ddabc0e
Ruslan Cheshko <chex@soul.li>
2017-03-13 18:50:09 +0200

    temp delete dializer from travis config

--- src/graphql_parser/graphql_parser.erl
+++ src/graphql_parser/graphql_parser.erl
@@ -7,14 +7,14 @@
 ]).
 
 
--spec parse(binary() | list()) -> map().
+-spec parse(binary() | maybe_improper_list()) -> {'error', map()} | {'ok', map()}.
 parse(Q) when is_binary(Q) ->
   ListQ = binary_to_list(Q),
   parse_list(ListQ);
 parse(Q) when is_list(Q)->
   parse_list(Q).
 
--spec parse_list(list()) -> map().
+-spec parse_list(maybe_improper_list()) -> {'error', map()} | {'ok', map()}.
 parse_list(Document)->
   {ok, Tokens, _} = graphql_lexer:string(Document),
   graphql_parser_yecc:parse(Tokens).
